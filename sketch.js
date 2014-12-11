8644d51c21614053999a588e5ffd11729f9c2145
lhj73999 <ehgkfdl@naver.com>
2014-12-11 14:59:42 +0900

    ss

--- sketch.js
+++ sketch.js
@@ -3,17 +3,10 @@ var bg;
 var y = 0;
 
 function setup() {
-  // The background image must be the same size as the parameters
-  // into the size() method. In this program, the size of the image
-  // is 710x400 pixels.
+
   bg = loadImage("space.jpg");
-  img = loadImage("earth.png");
-  img2 = loadImage("moon.png");
-  img3 = loadImage("neptune.png");
-  img4 = loadImage("venus.png");
-  img5 = loadImage("mars.png");
-  img6 = loadImage("sun.png");
-  createCanvas(500, 500);
+  img = loadImage("moon.png");
+
 }
 
 function draw() {
@@ -28,12 +21,9 @@ function draw() {
 
    fill(229,211,136);
    noStroke()
-   image(img4,250 + Math.cos(timing*2*PI)*200,           // x좌표
-        250 + Math.sin(timing*2*PI)*200,       // y좌표
-        40,                                     // width
-        40);                                    // height
-   //타원궤도 행성
-   var duration = 5000;
-   var timing = (new Date()%duration)/duration;
+   image(img,250 + Math.cos(timing*2*PI)*200,       
+        250 + Math.sin(timing*2*PI)*200,      
+        40,                                   
+        40);                                 
 
 }
