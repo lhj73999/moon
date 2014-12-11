caf2515e6c4f92fa71d8b1e9d5b187f9a0d6e223
lhj73999 <ehgkfdl@naver.com>
2014-12-11 15:00:35 +0900

    d

--- sketch.js
+++ sketch.js
@@ -14,13 +14,10 @@ function draw() {
   
    fill(255,64,64);
    noStroke()
-   image(img6, 140,140,270,270);
-   //노란
+
    var duration = 4000;
    var timing = (new Date()%duration)/duration;
 
-   fill(229,211,136);
-   noStroke()
    image(img,250 + Math.cos(timing*2*PI)*200,       
         250 + Math.sin(timing*2*PI)*200,      
         40,                                   
