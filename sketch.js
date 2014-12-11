cec209b6ecf9d785d3d122000cb9242b22f79d36
lhj73999 <ehgkfdl@naver.com>
2014-12-11 15:02:33 +0900

    s

--- sketch.js
+++ sketch.js
@@ -1,19 +1,17 @@
 var img;
 var bg;
-var y = 0;
 
 function setup() {
   createCanvas(800,800);
   bg = loadImage("space.jpg");
   img = loadImage("moon.png");
-
 }
 
 function draw() {
    background(bg);
   
-   fill(255,64,64);
-   noStroke()
+   noFfill();
+   noStroke();
 
    var duration = 4000;
    var timing = (new Date()%duration)/duration;
